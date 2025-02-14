127ea03d4bce8fa75583e2658491f297638d9a81
jyjeong1704 <jyjeong1704@gmail.com>
2025-02-15 05:06:26 +0900

    Tab navigator(delete car yet)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import HomeScreen from './screens/HomeScreen';
-import VehiclesListScreen from './screens/VehiclesListScreen';
-
-const Stack = createStackNavigator();
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import AppNavigator from './navigation/AppNavigator';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Vehicles" component={VehiclesListScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigator />
+    </GestureHandlerRootView>
   );
 };
 
